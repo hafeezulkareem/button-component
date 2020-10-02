5ff3103fbc70d5c47f0d888b00143669408c1929
hafeezulkareem <hafeezulkareem20@gmail.com>
2020-10-02 20:33:41 +0530

    Add props and constants

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,8 +1,49 @@
-import React, { Component, ReactElement } from 'react'
+import React, { Component, ReactElement, ReactNode } from 'react'
+import { ClipLoader } from 'react-spinners'
 
-class Button extends Component {
+import { colors, shapes, sizes, types } from './constants'
+import { ButtonText, StyledButton } from './styledComponents'
+
+interface ButtonProps {
+   onClick: Function
+   disabled: boolean
+   loading: boolean
+   size: string
+   type: string
+   color: string
+   shape: string
+   startIcon: ReactNode
+   endIcon: ReactNode
+   disableShadow: boolean
+   className: string
+}
+
+class Button extends Component<ButtonProps> {
+   static defaultProps = {
+      onClick: () => {},
+      disabled: false,
+      loading: false,
+      size: sizes.medium,
+      type: types.default,
+      color: colors.default,
+      shape: shapes.square,
+      startIcon: null,
+      endIcon: null,
+      disableShadow: false,
+      className: '',
+   }
+
+   renderButtonChild = (): ReactNode => {
+      const { loading, children } = this.props
+      if (loading) {
+         return <ClipLoader size={15} color={'#123abc'} loading={loading} />
+      }
+      return <ButtonText>{children}</ButtonText>
+   }
+
    render(): ReactElement {
-      return <div></div>
+      const { size, type, color, shape, disableShadow } = this.props
+      return <StyledButton>{this.renderButtonChild()}</StyledButton>
    }
 }
 
--- src/Button/styledComponents.ts
+++ src/Button/styledComponents.ts
@@ -1,7 +1,25 @@
 import tw, { styled } from 'twin.macro'
 
-export const StyledComponent = styled.div`
+export const StyledButton = styled.button`
    ${tw`
 
+    `}
+`
+
+export const ButtonText = styled.span`
+   ${tw`
+        text-16px
+    `}
+`
+
+export const StartIconContainer = styled.div`
+   ${tw`
+        mr-8px
+    `}
+`
+
+export const EndIconContainer = styled.div`
+   ${tw`
+        mr-8px
     `}
 `
