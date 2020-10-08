a3d02d5c73f4f72df8f83bac23adab6639dbca8f
hafeezulkareem <hafeezulkareem20@gmail.com>
2020-10-08 15:41:52 +0530

    Add button stories

---
+++ src/Button/Button.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { Button } from '.'
+
+export default {
+   title: 'Button',
+   argTypes: {
+      size: {
+         control: { type: 'select', options: ['SMALL', 'MEDIUM', 'LARGE'] },
+      },
+      variant: {
+         control: { type: 'select', options: ['DEFAULT', 'OUTLINE', 'TEXT'] },
+      },
+      color: {
+         control: {
+            type: 'select',
+            options: [
+               'DEFAULT',
+               'PRIMARY',
+               'SECONDARY',
+               'SUCCESS',
+               'WARNING',
+               'DANGER',
+            ],
+         },
+      },
+      shape: {
+         control: { type: 'select', options: ['ROUND', 'SQUARE', 'PILL'] },
+      },
+   },
+}
+
+export const AllInOneButton = (args) => <Button {...args}>All In One</Button>
